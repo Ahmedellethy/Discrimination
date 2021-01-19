93c7707ee5207a093af70a023e836265ef5988ec
Ahmed Lethy <76906123+Ahmedellethy@users.noreply.github.com>
2021-01-19 16:05:43 +0200

    Add files via upload

---
+++ IU.m
@@ -0,0 +1,96 @@
+% Initiate and Update The centers of the SOMs clusters
+
+%Read the seismic event parameters data in the folowing format
+% the depth of expolsions are zero
+%depth	PcfHz	ScfHz	PSRcf
+%08.46	10.28	08.95	1.15
+%the data are tab delimited
+addpath(genpath('C:\code'))
+formatspec= '%f%f%f%f';
+% spacifiy the data file
+Fld='C:\Code\Training.dat'; % Change the path as approperate
+%Read the data into table T
+T=readtable(Fld,'Delimiter','	','Format',formatspec);
+Depth=T{:,1}; 
+PSRcf=T{:,2:4};
+Pcf=PSRcf(:,1); %P-wave corner freq
+Scf=PSRcf(:,2); %S-wave corner freq
+Rcf=PSRcf(:,3); %Ratio of P-wave to S-wave corner freq
+Tgt=Depth(:)~=0; %Target binary set  1 for earthquakes and 0 for explosions
+Tgt=double(Tgt);
+sz=size(Pcf);
+TE=sz(1); %number of events
+% Cluster the data using the SOM
+SOMC1=SOM1(Pcf);
+SOMC2=SOM2(Scf);
+SOMC3=SOM3(Rcf);
+SOMC4=SOM4(PSRcf);
+nE1=sum(SOMC1);% number of events “hits” (nE) in each cluster
+nE2=sum(SOMC2);
+nE3=sum(SOMC3);
+nE4=sum(SOMC4);
+%Cluster support value SC=nE/TE equ. (5)
+SC1=nE1/TE;
+SC2=nE2/TE;
+SC3=nE3/TE;
+SC4=nE4/TE;
+% Create arrays for Tables 3 & 4 which contains the clusters details
+% No of events, no of earthquakes, no of explosions, Support, earthquakes
+% confidence, and explosions confidence
+C4P=zeros(6,4);
+C4S=zeros(6,4);
+C4R=zeros(6,4);
+C9PSR=zeros(6,9);
+% create arrays to hold the k-mean value of each cluster 
+K4P=zeros(4,1);
+K4S=zeros(4,1);
+K4R=zeros(4,1);
+K9=zeros(9,3);
+
+C4P(1,:)=nE1;
+C4P(4,:)=SC1;
+C4S(1,:)=nE2;
+C4S(4,:)=SC2;
+C4R(1,:)=nE3;
+C4R(4,:)=SC3;
+C9PSR(1,:)=nE4;
+C9PSR(4,:)=SC4;
+for i=1 : 4
+    Cx1=SOMC1(:,i)==1;
+    K4P(i)=mean(Pcf(Cx1));% centers
+    Ceq=Tgt(Cx1)==1 ;
+    Cex=Tgt(Cx1)==0;
+    C4P(2,i)=sum(Ceq);
+    C4P(3,i)=sum(Cex);
+    C4P(5,i)=C4P(2,i)/C4P(1,i); % CfEq=nEq/nE equ. (6)
+    C4P(6,i)=C4P(3,i)/C4P(1,i); % CfEx=nEx/nE equ. (7)
+    
+    Cx1=SOMC2(:,i)==1;
+    K4S(i)=mean(Scf(Cx1));% centers
+    Ceq=Tgt(Cx1)==1 ;
+    Cex=Tgt(Cx1)==0;
+    C4S(2,i)=sum(Ceq);
+    C4S(3,i)=sum(Cex);
+    C4S(5,i)=C4S(2,i)/C4S(1,i); % CfEq=nEq/nE equ. (6)
+    C4S(6,i)=C4S(3,i)/C4S(1,i); % CfEx=nEx/nE equ. (7)
+    
+    Cx1=SOMC3(:,i)==1;
+    K4R(i)=mean(Rcf(Cx1));% centers
+    Ceq=Tgt(Cx1)==1 ;
+    Cex=Tgt(Cx1)==0;
+    C4R(2,i)=sum(Ceq);
+    C4R(3,i)=sum(Cex);
+    C4R(5,i)=C4R(2,i)/C4R(1,i); % CfEq=nEq/nE equ. (6)
+    C4R(6,i)=C4R(3,i)/C4R(1,i); % CfEx=nEx/nE equ. (7)
+end 
+
+for i=1 : 9
+    Cx1=SOMC4(:,i)==1;
+    K9(i,:)=mean(PSRcf(Cx1,:)); % centers of the 9 clusters
+    Ceq=Tgt(Cx1)==1 ;
+    Cex=Tgt(Cx1)==0;
+    C9PSR(2,i)=sum(Ceq);
+    C9PSR(3,i)=sum(Cex);
+    C9PSR(5,i)=C9PSR(2,i)/C9PSR(1,i); % CfEq=nEq/nE equ. (6)
+    C9PSR(6,i)=C9PSR(3,i)/C9PSR(1,i); % CfEx=nEx/nE equ. (7)
+end
---
+++ MD.m
@@ -0,0 +1,88 @@
+% Machine Discriminator (MD) script
+% Written by: Ahmed Lethy
+% alethy@nriag.sci.eg         ahmedellethy@gmail.com
+% National Research Institute of Astronomy and Geophysics (NRIAG), Helwan, Egypt.
+%Read the seismic event parameters data in the folowing format
+%PcfHz	ScfHz	PSRcf
+%10.28	08.95	1.15
+%the data are tab delimited
+addpath(genpath('C:\code'))
+formatspec= '%f%f%f';
+% spacifiy the data file
+Fld='C:\Code\test-data.dat'; % Change the path as approperate
+%Read the data into table T
+Tt=readtable(Fld,'Delimiter','	','Format',formatspec); 
+PSRcf=Tt{:,:};
+Pcf=PSRcf(:,1); %P-wave corner freq
+Scf=PSRcf(:,2); %S-wave corner freq
+Rcf=PSRcf(:,3); %Ratio of P-wave to S-wave corner freq
+sz=size(Pcf);
+TE=sz(1); %number of events
+%This data is feed to the ANNs presented in Figure 4 and listed in Table 2.
+ANN1=net1(Pcf')';
+ANN2=net2(Pcf')';
+ANN3=net3(Scf')';
+ANN4=net4(Scf')';
+ANN5=net5(Rcf')';
+ANN6=net6(Rcf')';
+ANN7=net7(PSRcf')';
+ANN8=net8(PSRcf')';
+% the results are combined using Eq. (4). 
+ANNC1=ANN1>2 & ANN2>0.5;
+ANNC2=ANN3>2 & ANN4>0.5;
+ANNC3=ANN5>2 & ANN6>0.5;
+ANNC4=ANN7>2 & ANN8>0.5;
+% finding the holding clusters
+CMin=zeros(TE,12);
+CMin(:,1)=ANNC1;
+CMin(:,2)=ANNC2;
+CMin(:,3)=ANNC3;
+CMin(:,4)=ANNC4;
+
+for i= 1 : TE
+        dd=pdist2(K4P,Pcf(i,:));
+        [xm,ik]=min(dd);
+        CMin(i,5)=C4P(4,ik);% support measure
+        if ANNC1(i)==1
+            CMin(i,6)=C4P(5,ik); % confidence of earthquake
+        else
+            CMin(i,6)=C4P(6,ik); % confidence of explosion
+        end
+    
+        dd=pdist2(K4S,Scf(i,:));
+        [xm,ik]=min(dd);
+        CMin(i,7)=C4S(4,ik);% support measure
+        if ANNC2(i)==1
+            CMin(i,8)=C4S(5,ik); % confidence of earthquake
+        else
+            CMin(i,8)=C4S(6,ik); % confidence of explosion
+        end
+    
+        dd=pdist2(K4R,Rcf(i,:));
+        [xm,ik]=min(dd);
+        CMin(i,9)=C4R(4,ik);% support measure
+        if ANNC3(i)==1
+            CMin(i,10)=C4R(5,ik); % confidence of earthquake
+        else
+            CMin(i,10)=C4R(6,ik); % confidence of explosion
+        end
+        
+        dd=pdist2(K9,PSRcf(i,:));
+        [xm,ik]=min(dd);
+        CMin(i,11)=C9PSR(4,ik);% support measure
+        if ANNC4(i)==1
+            CMin(i,12)=C9PSR(5,ik); % confidence of earthquake
+        else
+            CMin(i,12)=C9PSR(6,ik); % confidence of explosion
+        end
+end
+CMout=CMnet(CMin);
+CMout(:,2:3)=CMout(:,2:3)*100; % present the support and confidence as pecentage
+% Save the results to CM.txt file
+% The results are the event type in the 1st col where 1 for earthquakes and 0 for explosions
+% The Support value in the 2nd col and the confidence are in the 3rd col
+fid = fopen('C:\Code\CM.txt','w');
+    for row=1:TE
+        fprintf(fid,'%1u\t%4.2f\t%4.2f\r\n',CMout(row,:));
+    end
+fclose(fid);
